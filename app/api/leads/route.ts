0d7be976568c297c0a38d5fe359131730ec444f3
Luiz Pereira <luckpecontato@gmail.com>
2023-06-10 01:49:49 -0300

    feat(api): creating POST route for lead generation

--- app/api/leads/route.ts
+++ app/api/leads/route.ts
@@ -1,6 +1,56 @@
-
 import prisma from "@/libs/prisma";
 import { NextResponse } from "next/server";
+import { z } from "zod";
+
+type Ilead = {
+    nome: string;
+    mail: string;
+    telefone: string;
+    status: string;
+    descricao: string;
+};
+
+async function createLead(lead: Ilead) {
+  const newLead = await prisma.lead.create({
+    data: lead,
+  });
+  return newLead;
+}
+
+const phoneRegex = new RegExp(
+  /^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/
+);
+
+const schema = z.object({
+  nome: z.string(),
+  mail: z.string().email("e-mail invalido."),
+  telefone: z.string().regex(phoneRegex, "Numero invalido!"),
+  status: z.string(),
+  descricao: z.string(),
+});
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  const response = schema.safeParse(body);
+
+  if (!response.success) {
+    const { errors } = response.error;
+
+    return NextResponse.json(
+      {
+        error: { message: "Invalid request", errors },
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  createLead(response.data);
+
+  return NextResponse.json(response);
+}
 
 const getLeads = () => {
   return prisma.lead.findMany();
