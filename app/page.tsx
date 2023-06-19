f5e074ad722b53c98ca54081c7ec66c4d798b91f
luizpereiradev <luckpecontato@gmail.com>
2023-06-19 13:10:45 -0300

    build: initial migration

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import LeadsTable from "@/components/leadsTable";
-import { NewLeadForm } from "@/components/newLeadForm";
-
-export default function Home() {
-  return (<>
-    <LeadsTable />
-    <NewLeadForm />
-  </>);
-}
