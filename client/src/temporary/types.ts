3a75531141546571e301a5eaa40682e99a9ec6f3
Pedro Henrique Alves Farias <pedro.alves.hf@gmail.com>
2023-06-21 15:08:17 -0300

    chore(frontend client): installed dependencies
    installed clsx, input mask, zod and react-hook form

---
+++ client/src/temporary/types.ts
@@ -0,0 +1,12 @@
+export type ILead = {
+  name: string;
+  email: string;
+  phone: string;
+  status?: string;
+  description?: string;
+};
+
+export type ILeadId = ILead & {
+  id: number;
+  createdAt: Date;
+};
